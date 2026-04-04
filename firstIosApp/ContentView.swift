f8a13523f247786b6e5d243ffe56a582891c52b2
kishore kumar <114152420+kishorekumawath@users.noreply.github.com>
2026-04-04 16:23:49 +0530

    Initial Commit

---
+++ firstIosApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  firstIosApp
+//
+//  Created by Kishore Kumar on 04/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ firstIosApp/firstIosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstIosAppApp.swift
+//  firstIosApp
+//
+//  Created by Kishore Kumar on 04/04/26.
+//
+
+import SwiftUI
+
+@main
+struct firstIosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
