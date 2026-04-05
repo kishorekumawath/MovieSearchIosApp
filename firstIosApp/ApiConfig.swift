6c2238b2b52611e8d2d5ec09c600f3e8903d3262
kishore kumar <114152420+kishorekumawath@users.noreply.github.com>
2026-04-05 22:33:48 +0530

    added the Error msg and api config

---
+++ firstIosApp/ApiConfig.swift
@@ -0,0 +1,37 @@
+//
+//  ApiConfig.swift
+//  firstIosApp
+//
+//  Created by Kishore Kumar on 05/04/26.
+//
+
+import Foundation
+struct APIConfig: Decodable{
+    let traktBaseURL: String
+    let traktClientID: String
+    
+    static let shared: APIConfig? = {
+        do{
+            return try loadConfig()
+        }catch{
+            print("Failed to load API config: \(error.localizedDescription)")
+                       return nil
+        }
+    }()
+    
+    private static func loadConfig() throws -> APIConfig {
+           guard let url = Bundle.main.url(forResource: "APIConfig", withExtension: "json") else {
+               throw APIConfigError.fileNotFound
+           }
+           
+           do {
+               let data = try Data(contentsOf: url)
+               return try JSONDecoder().decode(APIConfig.self, from: data)
+           } catch let error as DecodingError {
+               throw APIConfigError.decodingFailed(underlyingError: error)
+           } catch {
+               throw APIConfigError.dataLoadingFailed(underlyingError: error)
+           }
+       }
+    
+}
